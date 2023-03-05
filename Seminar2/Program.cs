06b14281870999177d0685705bfeeb2b120143dc
Andrey <andreynapalkov777@example.com>
2023-03-05 20:45:50 +0300

    add

--- Seminar2/Program.cs
+++ Seminar2/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая выводит случайное 
+//трёхзначное число и удаляет вторую цифру этого числа.
+/*
+int CatNumber(int num)
+{
+    int hundreds = num / 100;
+    int ones = num % 10;
+    int result = hundreds * 10 + ones;
+    return result;
+}
+
+int randNumber = new Random().Next(100, 1000);
+
+int newNumber = CatNumber(randNumber);
+Console.WriteLine($"New version of {randNumber} is {newNumber}");
+*/
+//Напишите программу, которая принимает на вход число
+// и проверяет, кратно ли оно одновременно a и b.
+/*
+bool isDivided(int number, int firstDivider, int secondDivider)
+{
+    if(number % firstDivider == 0 && number % secondDivider == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+Console.WriteLine("Input a number for checking: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input a first divider: ");
+int div1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input a second divider: ");
+int div2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(isDivided(num, div1, div2));
+*/
