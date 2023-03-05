8f1033ffc63f364d10a19c7c15e8e88c4c30623f
Andrey <andreynapalkov777@example.com>
2023-03-05 20:45:44 +0300

    add

---
+++ lection/Program.cs
@@ -0,0 +1,34 @@
+﻿/*int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int[] array = {11, 23, 56, 89, 23, 56 ,22, 11, 33};
+array[0] = 12;
+Console.WriteLine(array[4]);
+
+
+int[] array = {1, 2, 4, 45, 67, 78, 23, 56 }; 
+
+int n = array.Length;
+int find = 4; 
+
+int index = 0;
+
+while (index < n)
+{
+   if(array[index] == find)
+   {
+    Console.WriteLine(index);
+   } 
+   index++;
+}
+*/
+void FillArray
+
+
+
+int[] array = new int[10]
