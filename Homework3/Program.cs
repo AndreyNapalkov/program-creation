eb9225e3af9b0a2f5064954c9d3adfacc9942f89
Andrey <andreynapalkov777@example.com>
2023-03-08 00:35:12 +0300

    1

--- Homework3/Program.cs
+++ Homework3/Program.cs
@@ -26,10 +26,42 @@ System.Console.WriteLine($"Расстояние между точками = {dis
 
 //Напишите программу, которая принимает на вход число (N)
 // и выдаёт таблицу кубов чисел от 1 до N.
-
+/*
 Console.Write("Введите число: ");
 double number = Convert.ToDouble(Console.ReadLine());
 for(int i = 1; i<=number; i++)
 {
-    Console.Write($"{Math.Pow(i,3)}");
+    Console.Write($"{Math.Pow(i,3)} ");
 }
+*/
+
+//Напишите программу, которая принимает на вход пятизначное число
+// и проверяет, является ли оно палиндромом
+/*
+void CheckPalindromicNumber(int number)
+{
+    if(number >= 10000)
+    {
+        int division1 = number / 10000;
+        int remainder1 = number % 10;
+ 
+            if(division1 == remainder1)
+            {
+                number = number / 10;
+                int division2 = (number / 100) % 10;
+                int remainder2 = number % 10;
+                if(division2 == remainder2)
+                    Console.WriteLine("Да");
+            }
+            else 
+            Console.WriteLine("Нет");
+            
+    }
+    else
+    Console.WriteLine("Некорректное число!");
+}
+ 
+Console.WriteLine("Введите пятизначное число:");
+int number = int.Parse(Console.ReadLine()!);
+CheckPalindromicNumber(number);
+*/
