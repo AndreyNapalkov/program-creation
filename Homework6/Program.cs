e3a21d9bfec6d628908f059d3c3f43c2ce0d9680
Andrey <andreynapalkov777@example.com>
2023-03-19 15:03:33 +0300

    1

--- Homework6/Program.cs
+++ Homework6/Program.cs
@@ -27,26 +27,26 @@
 
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
-Console.WriteLine("Введите уравнение первой прямой (y = k1 * x + b1):");
-Console.Write("k1: ");
-double k1 = Convert.ToDouble(Console.ReadLine());
-Console.Write("b1: ");
-double b1 = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Введите уравнение первой прямой (y = k1 * x + b1):");
+// Console.Write("k1: ");
+// double k1 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("b1: ");
+// double b1 = Convert.ToDouble(Console.ReadLine());
 
-Console.WriteLine("Введите уравнение второй прямой (y = k2 * x + b2):");
-Console.Write("k2: ");
-double k2 = Convert.ToDouble(Console.ReadLine());
-Console.Write("b2: ");
-double b2 = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Введите уравнение второй прямой (y = k2 * x + b2):");
+// Console.Write("k2: ");
+// double k2 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("b2: ");
+// double b2 = Convert.ToDouble(Console.ReadLine());
 
-if (k1 == k2) 
-{
-    Console.WriteLine("Прямые параллельны, точек пересечения нет");
-} 
-else 
-{
-    double x = (b2 - b1) / (k1 - k2);
-    double y = k1 * x + b1;
+// if (k1 == k2) 
+// {
+//     Console.WriteLine("Прямые параллельны, точек пересечения нет");
+// } 
+// else 
+// {
+//     double x = (b2 - b1) / (k1 - k2);
+//     double y = k1 * x + b1;
 
-    Console.WriteLine($"Точка пересечения прямых: ({x}, {y})");
-}
+//     Console.WriteLine($"Точка пересечения прямых: ({x}, {y})");
+// }
